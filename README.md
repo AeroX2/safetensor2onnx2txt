4f953c377408994f126f70980967c5b0fe63bbda
vitoplantamura <plantamura@gmail.com>
2025-08-28 10:15:39 +0200

    Add option to also export the text encoders and the VAE decoder

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 ﻿# safetensor2onnx2txt
 
-Run `convert.py`, this will then ask for a `*.safetensors` file which is the SDXL Turbo model you want to load.
-The program will then generate a `sdxl_unet_fp16` folder which can then be used to replace the folder in https://huggingface.co/AeroX2/stable-diffusion-xl-turbo-1.0-onnxstream
+This project allows to export a checkpoint (+LoRA optionally) of SDXL (Base or Turbo) to the [OnnxStream](https://github.com/vitoplantamura/OnnxStream) compatible text format.
 
-Note:
-- Some models don't seem to work, no idea why, still investigating
+Run `convert.py`, this will then ask for a `*.safetensors` file which is the SDXL model you want to load.
+The program will then generate one or more folders which can then be used to replace the folders in https://huggingface.co/vitoplantamura/stable-diffusion-xl-base-1.0-onnxstream or https://huggingface.co/vitoplantamura/stable-diffusion-xl-turbo-1.0-anyshape-onnxstream.
--- convert.py
+++ convert.py
@@ -11,9 +11,6 @@ import tkinter as tk
 from tkinter import simpledialog, filedialog, messagebox
 from pathlib import Path
 
-root = tk.Tk()
-root.withdraw()
-
 safetensor_path = filedialog.askopenfilename()
 safetensor_path = Path(safetensor_path)
 
@@ -26,37 +23,115 @@ if lora:
     lora_path = Path(lora_path)
     lora_weight = float(simpledialog.askfloat("Lora weight", "Please enter the lora weight between 0 and 1:"))
 
-onnx_path = safetensor_path.parent / "{}_{}".format(safetensor_path.stem, 'onnx') / safetensor_path.with_suffix('.onnx').name
-#text_encoder_2_path = safetensor_path.parent / "{}_{}".format(safetensor_path.stem, 'text_encoder_2') / safetensor_path.with_suffix('.onnx').name
-simp_onnx_path = safetensor_path.parent / "{}_{}".format(safetensor_path.stem, 'onnx') / safetensor_path.with_suffix('.sim.onnx').name
-#simp_text_encoder_2_path = safetensor_path.parent / "{}_{}".format(safetensor_path.stem, 'text_encoder_2') / safetensor_path.with_suffix('.sim.onnx').name
-unet_txt_path = safetensor_path.parent / 'sdxl_unet_fp16'
-#text_encoder_2_txt_path = safetensor_path.parent / 'sdxl_text_encoder_2_fp32'
+root = tk.Tk()
+root.title("Models to export:")
+checkbox_states = {
+    "text_encoder_1": tk.IntVar(value=1),
+    "text_encoder_2": tk.IntVar(value=1),
+    "unet_model": tk.IntVar(value=1),
+    "vae_decoder": tk.IntVar(value=1)
+}
+checkbox_frame = tk.Frame(root, padx=100, pady=10)
+checkbox_frame.pack()
+tk.Checkbutton(checkbox_frame, text="Text Encoder 1", variable=checkbox_states["text_encoder_1"]).pack(anchor="w")
+tk.Checkbutton(checkbox_frame, text="Text Encoder 2", variable=checkbox_states["text_encoder_2"]).pack(anchor="w")
+tk.Checkbutton(checkbox_frame, text="UNET Model", variable=checkbox_states["unet_model"]).pack(anchor="w")
+tk.Checkbutton(checkbox_frame, text="VAE Decoder", variable=checkbox_states["vae_decoder"]).pack(anchor="w")
 
-onnx_path.parent.mkdir(exist_ok=True)
-#text_encoder_2_path.parent.mkdir(exist_ok=True)
+onnx_path = None
+vaed_path = None
+text_encoder_1_path = None
+text_encoder_2_path = None
 
-safetensor2onnx(safetensor_path, lora_path, lora_weight, onnx_path) #, text_encoder_2_path)
+def on_ok_button_click():
+    global onnx_path, vaed_path, text_encoder_1_path, text_encoder_2_path
+    if bool(checkbox_states['unet_model'].get()):
+        onnx_path = safetensor_path.parent / "{}_{}".format(safetensor_path.stem, 'onnx') / safetensor_path.with_suffix('.onnx').name
+    if bool(checkbox_states['vae_decoder'].get()):
+        vaed_path = safetensor_path.parent / "{}_{}".format(safetensor_path.stem, 'vaed') / safetensor_path.with_suffix('.onnx').name
+    if bool(checkbox_states['text_encoder_1'].get()):
+        text_encoder_1_path = safetensor_path.parent / "{}_{}".format(safetensor_path.stem, 'text_encoder_1') / safetensor_path.with_suffix('.onnx').name
+    if bool(checkbox_states['text_encoder_2'].get()):
+        text_encoder_2_path = safetensor_path.parent / "{}_{}".format(safetensor_path.stem, 'text_encoder_2') / safetensor_path.with_suffix('.onnx').name
+    root.destroy()
 
-input_shape = '{"sample":[1,4,64,64],"timestep":[1],"encoder_hidden_state":[1,77,2048],"text_embeds":[1,1280],"time_ids":[1,6]}'
-simplify_large_onnx(AttrDict({
-    "in_model_path": onnx_path,
-    "out_model_path": simp_onnx_path, 
-    "input_shape": input_shape,
-    "size_th_kb": 1024,
-    "save_extern_data": 1,
-    "skip": "",
-}))
+tk.Button(root, text="OK", command=on_ok_button_click).pack(pady=5)
+root.update_idletasks()
+window_width = root.winfo_width()
+window_height = root.winfo_height()
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+x_pos = int((screen_width - window_width) / 2)
+y_pos = int((screen_height - window_height) / 2)
+root.geometry(f"{window_width}x{window_height}+{x_pos}+{y_pos}")
+root.mainloop()
 
-#input_shape = '{"input_ids": [1,77]}'
-#simplify_large_onnx(AttrDict({
-#    "in_model_path": text_encoder_2_path,
-#    "out_model_path": simp_text_encoder_2_path, 
-#    "input_shape": input_shape,
-#    "size_th_kb": 1024,
-#    "save_extern_data": 1,
-#    "skip": "",
-#}))
+simp_onnx_path = None if not onnx_path else safetensor_path.parent / "{}_{}".format(safetensor_path.stem, 'onnx') / safetensor_path.with_suffix('.sim.onnx').name
+simp_vaed_path = None if not vaed_path else safetensor_path.parent / "{}_{}".format(safetensor_path.stem, 'vaed') / safetensor_path.with_suffix('.sim.onnx').name
+simp_text_encoder_1_path = None if not text_encoder_1_path else safetensor_path.parent / "{}_{}".format(safetensor_path.stem, 'text_encoder_1') / safetensor_path.with_suffix('.sim.onnx').name
+simp_text_encoder_2_path = None if not text_encoder_2_path else safetensor_path.parent / "{}_{}".format(safetensor_path.stem, 'text_encoder_2') / safetensor_path.with_suffix('.sim.onnx').name
 
-onnx2txt(simp_onnx_path, unet_txt_path)
-#onnx2txt(simp_text_encoder_2_path, text_encoder_2_txt_path, fp16=False)
+unet_txt_path = None if not onnx_path else safetensor_path.parent / 'sdxl_unet_fp16'
+vaed_txt_path = None if not vaed_path else safetensor_path.parent / 'sdxl_vae_decoder_fp16'
+text_encoder_1_txt_path = None if not text_encoder_1_path else safetensor_path.parent / 'sdxl_text_encoder_1_fp32'
+text_encoder_2_txt_path = None if not text_encoder_2_path else safetensor_path.parent / 'sdxl_text_encoder_2_fp32'
+
+if onnx_path:
+    onnx_path.parent.mkdir(exist_ok=True)
+if vaed_path:
+    vaed_path.parent.mkdir(exist_ok=True)
+if text_encoder_1_path:
+    text_encoder_1_path.parent.mkdir(exist_ok=True)
+if text_encoder_2_path:
+    text_encoder_2_path.parent.mkdir(exist_ok=True)
+
+safetensor2onnx(safetensor_path, lora_path, lora_weight, onnx_path, vaed_path, text_encoder_1_path, text_encoder_2_path)
+
+if onnx_path:
+    simplify_large_onnx(AttrDict({
+        "in_model_path": onnx_path,
+        "out_model_path": simp_onnx_path, 
+        "input_shape": "",
+        "size_th_kb": 1024,
+        "save_extern_data": 1,
+        "skip": "",
+    }))
+
+if vaed_path:
+    simplify_large_onnx(AttrDict({
+        "in_model_path": vaed_path,
+        "out_model_path": simp_vaed_path, 
+        "input_shape": "",
+        "size_th_kb": 1024,
+        "save_extern_data": 1,
+        "skip": "",
+    }))
+
+if text_encoder_1_path:
+    simplify_large_onnx(AttrDict({
+        "in_model_path": text_encoder_1_path,
+        "out_model_path": simp_text_encoder_1_path, 
+        "input_shape": "",
+        "size_th_kb": 1024,
+        "save_extern_data": 1,
+        "skip": "",
+    }))
+
+if text_encoder_2_path:
+    simplify_large_onnx(AttrDict({
+        "in_model_path": text_encoder_2_path,
+        "out_model_path": simp_text_encoder_2_path, 
+        "input_shape": "",
+        "size_th_kb": 1024,
+        "save_extern_data": 1,
+        "skip": "",
+    }))
+
+if onnx_path:
+    onnx2txt(simp_onnx_path, unet_txt_path)
+if vaed_path:
+    onnx2txt(simp_vaed_path, vaed_txt_path)
+if text_encoder_1_path:
+    onnx2txt(simp_text_encoder_1_path, text_encoder_1_txt_path, fp16=False)
+if text_encoder_2_path:
+    onnx2txt(simp_text_encoder_2_path, text_encoder_2_txt_path, fp16=False)
